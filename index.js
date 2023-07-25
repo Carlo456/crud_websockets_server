a42911ce22c6444f8154c0614c546b2353a42557
Carlo456 <gusvalenzuela77@gmail.com>
2023-07-25 15:41:58 -0600

    added broadcast connection

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ import { server_config } from './config.js';
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new SocketServer(server, {
+    cors: "*"
+});
+
+io.on('connection', (socket) => {
+    console.log(`Client connected: ${socket}`);
+
+    socket.on('message', (data) => {
+        const responseMessage = {
+            body: data,
+            from: socket.id,
+        };
+        socket.broadcast.emit('responseMessage', responseMessage);
+    });
+});
 
 server.listen(server_config.PORT,() => {
     console.log(`Server on port: ${server_config.PORT}`)
